48db1899e1f3a7c6e493c1771efdb413f1680fe2
sawyer47altf4 <68043480+sawyer47altf4@users.noreply.github.com>
2021-04-15 16:25:55 -0500

    not done
    not done with code and debug

---
+++ r-p-s-im/r-p-s-impos.py
@@ -0,0 +1,16 @@
+
+print ("welcom")
+
+main = input("p for play or e for stop code list : ")
+
+if main == e:
+    print("stop code")
+    print("3805 = place holder")
+
+elif main == p:
+    print("3")
+    print("2")
+    print("1")
+    print("go")
+
+    loser = input("(pick r p s)  : ")
