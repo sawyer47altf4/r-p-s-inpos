804bec6c5a926e42de4757e1d9697fb1000ff890
sawyer47altf4 <68043480+sawyer47altf4@users.noreply.github.com>
2021-04-16 01:29:31 -0500

    idk

--- r-p-s-im/r-p-s-impos.py
+++ r-p-s-im/r-p-s-impos.py
@@ -1,16 +1,32 @@
-
+#sawyer47
 print ("welcom")
 
-main = input("p for play or e for stop code list : ")
+main = input("p for play or e for stop codes : ")
 
-if main == e:
-    print("stop code")
-    print("3805 = place holder")
+if main == "e":
+    print("stop codes")
+    print("404 = not fand")
 
-elif main == p:
+elif main == "p":
     print("3")
     print("2")
     print("1")
     print("go")
-
     loser = input("(pick r p s)  : ")
+#sawyer47
+    if loser == "r":
+        print("i pick p")
+        print("you r a loser")
+
+    elif loser == "p":
+        print("i pick s")
+        print("you r a loser")
+
+    elif loser == "s":
+        print("i pick r")
+        print("you r a loser")
+    else:
+        print("erre code 404")
+else:
+    print("erre code 404")
+    #sawyer47
